341f83f4c3cb3a95013fab91141513b796946ccc
Daniel Johansson <donnex@donnex.net>
2014-12-07 19:47:20 +0100

    Serve static and media files with nginx instead of Django
    - Disable sendfile in Nginx to fix a problem with static files when running Docker in Virtualbox

--- {{ cookiecutter.repo_name }}/web/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{ cookiecutter.repo_name }}/web/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
-from django.conf import settings
 from django.views.generic import TemplateView
 
 admin.autodiscover()
@@ -12,10 +10,3 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
 )
-
-# This is only needed when using runserver.
-if settings.DEBUG:
-    urlpatterns = patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  # NOQA
-            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-        ) + staticfiles_urlpatterns() + urlpatterns  # NOQA
