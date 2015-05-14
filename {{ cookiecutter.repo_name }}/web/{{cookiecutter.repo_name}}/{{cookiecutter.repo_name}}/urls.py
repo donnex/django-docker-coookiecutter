ef3a3ddf43d9ea3638f2de306a10e590af179af6
Daniel Johansson <donnex@donnex.net>
2015-05-14 10:33:54 +0200

    Django urls.py updates
    - Patterns not needed anymore
    - Change urlpatterns to a list
    - admin.autodiscover() is enabled by default

--- {{ cookiecutter.repo_name }}/web/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
+++ {{ cookiecutter.repo_name }}/web/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-admin.autodiscover()
-
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='base.html')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
